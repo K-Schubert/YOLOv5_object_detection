74bc9f1f4af4e76d360d0b0ac801034a4c6a1252
Kieran Schubert <schubert.kieran@gmail.com>
2020-08-13 16:28:55 +0200

    added plot_images

---
+++ scripts/plot_images.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import random
+
+'''
+This module samples images from the dataset and creates a mosaic for display purposes.
+'''
+
+n_images = 3
+image_dirs = os.listdir("../data/birds/")[1:4]
+sampled_dirs = random.sample(image_dirs, n_images)
+
+fig, axs = plt.subplots(3,3,figsize=(6,6))
+
+axs = axs.ravel()
+
+for i in range(n_images):
+	img = mpimg.imread(f'../data/birds/{sampled_dirs[i]}/00000000.jpg')
+	axs[i].axis('off')
+	axs[i].grid(None)
+	axs[i].imshow(img, aspect='auto')
+
+plt.subplots_adjust(wspace=0, hspace=0)
+
+plt.savefig("../plots/mosaic.jpg")
+plt.show()
+
+
+
