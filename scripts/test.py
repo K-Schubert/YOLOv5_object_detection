af678986a1f0bcaf322cb439e18b2258e854788b
Kieran Schubert <schubert.kieran@gmail.com>
2020-08-13 14:49:37 +0200

    updated test script

--- scripts/test.py
+++ scripts/test.py
@@ -1,28 +1,10 @@
-import os
-import csv
+import urllib.request
+from bs4 import BeautifulSoup as BS
 
-
-path = 'data/birds/'
-folders = os.listdir(path)[1:]
-
-for species in folders:
-	
-	folder = species + "/"
-	file = os.listdir(path + folder)
-
-	# Check for hidden .DS_Store file
-	if len(file) > 1:
-		file = file[1]
-	else:
-		file = file[0]
-
-	temp_csv_file = file[:-4] + '_tmp' + file[-4:]
-
-	with open(path+folder+file, 'r') as inp, open(path+folder+temp_csv_file, 'w') as out:
-		writer = csv.writer(out)
-		for row in csv.reader(inp):
-			if row and 'www.ornitho.ch/2020' in row:
-				writer.writerow(row)
-
-	os.remove(path+folder+file)
-	os.rename(path+folder+temp_csv_file, path+folder+file)
+url = "https://www.ornitho.ch/index.php?m_id=15"
+soup = BS(urllib.request.urlopen(url), 'lxml')
+species = [str(x.text) for x in soup.find_all('option')]
+print(len(species))
+main_cat = [s.split(' ', 1)[0] for s in species]
+print(set(main_cat))
+print(len(set(main_cat)))
