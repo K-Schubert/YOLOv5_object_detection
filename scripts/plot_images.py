792da934df974210f6f132af2e16fc44e5ab8cc8
Kieran Schubert <schubert.kieran@gmail.com>
2020-08-20 19:08:23 +0200

    updated plot_images.py

--- scripts/plot_images.py
+++ scripts/plot_images.py
@@ -2,21 +2,23 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 import random
+import glob
 
 '''
 This module samples images from the dataset and creates a mosaic for display purposes.
 '''
 
-n_images = 3
-image_dirs = os.listdir("../data/birds/")[1:4]
+n_images = 100
+image_dirs = os.listdir("../data/birds/")
 sampled_dirs = random.sample(image_dirs, n_images)
 
-fig, axs = plt.subplots(1,3,figsize=(6,2))
+fig, axs = plt.subplots(10,10,figsize=(6,6))
 
 axs = axs.ravel()
 
 for i in range(n_images):
-	img = mpimg.imread(f'../data/birds/{sampled_dirs[i]}/00000000.jpg')
+	jpg_file = glob.glob(f'../data/birds/{sampled_dirs[i]}/*.jpg')[0]
+	img = mpimg.imread(jpg_file)
 	axs[i].axis('off')
 	axs[i].grid(None)
 	axs[i].imshow(img, aspect='auto')
